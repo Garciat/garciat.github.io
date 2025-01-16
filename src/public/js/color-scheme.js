3f4cf14c02416fac44b1b19669dfa0daf123152b
Gabriel Garcia <garciat@uber.com>
2025-01-16 21:35:33 +0100

    Run deno fmt

--- src/public/js/color-scheme.js
+++ src/public/js/color-scheme.js
@@ -6,37 +6,45 @@ class UserColorScheme {
     localStorage.setItem("color-scheme", value);
   }
   static display(value, allValues, defaultValue) {
-    allValues.forEach(v => document.body.classList.remove(v));
+    allValues.forEach((v) => document.body.classList.remove(v));
     document.body.classList.add(value);
 
-    document.querySelectorAll('link[rel="stylesheet"][data-theme]').forEach((/** @type HTMLLinkElement */ element) => {
-      if (value === defaultValue) {
-        element.disabled = false;
-        element.media = element.dataset["media"];
-      } else {
-        element.disabled = element.dataset["theme"] !== value;
-        element.media = "";
-      }
-    });
+    document.querySelectorAll('link[rel="stylesheet"][data-theme]').forEach(
+      (/** @type HTMLLinkElement */ element) => {
+        if (value === defaultValue) {
+          element.disabled = false;
+          element.media = element.dataset["media"];
+        } else {
+          element.disabled = element.dataset["theme"] !== value;
+          element.media = "";
+        }
+      },
+    );
   }
 }
 
-document.querySelectorAll('link[rel="stylesheet"][data-theme]').forEach((/** @type HTMLLinkElement */ element) => {
-  element.dataset["media"] = element.media; // save media attribute
-});
+document.querySelectorAll('link[rel="stylesheet"][data-theme]').forEach(
+  (/** @type HTMLLinkElement */ element) => {
+    element.dataset["media"] = element.media; // save media attribute
+  },
+);
 
-document.querySelectorAll('#color-scheme-selector').forEach((/** @type HTMLSelectElement */ element) => {
-  const defaultValue = element.dataset["default"];
-  const allValues = Array.from(element.querySelectorAll('option')).map(e => e.value);
+document.querySelectorAll("#color-scheme-selector").forEach(
+  (/** @type HTMLSelectElement */ element) => {
+    const defaultValue = element.dataset["default"];
+    const allValues = Array.from(element.querySelectorAll("option")).map((e) =>
+      e.value
+    );
 
-  const userValue = UserColorScheme.read(defaultValue);
+    const userValue = UserColorScheme.read(defaultValue);
 
-  element.value = userValue;
-  UserColorScheme.display(userValue, allValues, defaultValue);
+    element.value = userValue;
+    UserColorScheme.display(userValue, allValues, defaultValue);
 
-  element.onchange = () => {
-    const newValue = element.value;
-    UserColorScheme.write(newValue);
-    UserColorScheme.display(newValue, allValues, defaultValue);
-  };
-});
+    element.onchange = () => {
+      const newValue = element.value;
+      UserColorScheme.write(newValue);
+      UserColorScheme.display(newValue, allValues, defaultValue);
+    };
+  },
+);
