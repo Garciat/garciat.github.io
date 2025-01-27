09ccb516d9208eaf370beafcb710ac7a8b09d772
Gabriel Garcia <garciat@uber.com>
2025-01-27 14:23:53 +0100

    Try blog post structured data in index page

--- src/index.page.tsx
+++ src/index.page.tsx
@@ -15,18 +15,24 @@ export const structuredData: WebSiteSD = {
 export default ({ search }: Lume.Data, h: Lume.Helpers) => {
   return (
     <>
-      <main class="container content">
+      <main
+        itemscope
+        itemtype="http://schema.org/Person"
+        itemid="#garciat"
+        class="container content"
+      >
+        <link itemprop="url" href={h.url("/about/")} />
         <header>
-          <h1>{title}</h1>
+          <h1 itemprop="name">{title}</h1>
         </header>
-        <section class="bio">
+        <section itemprop="description">
           <p>
             I'm a software developer based in Amsterdam who's been writing code
             for nearly 20 years. I am primarily a backend developer, but I have
             my roots in web development. Most recently, I have worked as a
             software engineer at <a href="https://www.uber.com/">Uber</a>{" "}
             for about 6 years. For more details, check out{" "}
-            <a href="https://www.linkedin.com/in/ggarciat/">
+            <a itemprop="sameAs" href="https://www.linkedin.com/in/ggarciat/">
               my LinkedIn profile
             </a>.
           </p>
@@ -38,12 +44,23 @@ export default ({ search }: Lume.Data, h: Lume.Helpers) => {
         </header>
         <ul class="post-index">
           {search.pages("type=post", "date=desc").map((post) => (
-            <li class="post-index-item">
-              <time class="col1 weak" datetime={post.date.toISOString()}>
+            <li
+              itemscope
+              itemtype="http://schema.org/BlogPosting"
+              class="post-index-item"
+            >
+              <link itemprop="author" href="#garciat" />
+              <time
+                itemprop="datePublished"
+                class="col1 weak"
+                datetime={post.date.toISOString()}
+              >
                 {h.date(post.date, "MMM dd, yyyy")}
               </time>
               <span class="separator">{" \u2014 "}</span>
-              <a class="col2" href={h.url(post.url)}>{post.title}</a>
+              <a itemprop="url" class="col2" href={h.url(post.url)}>
+                <span itemprop="headline">{post.title}</span>
+              </a>
             </li>
           ))}
         </ul>
